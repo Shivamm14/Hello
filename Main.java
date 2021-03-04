c63b8b02cdca23882a3a682dc64164b3d675b52c
shivamm14 <shivammishra14nov@gmail.com>
2021-03-04 10:43:16 +0530

    Refactored Main

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Team firstTeam = new Team("India");
-        Team secondTeam = new Team("Australia");
-        Game game = new Game(firstTeam, secondTeam);
-        game.start();
+        Game game = new Game(2);
+        game.buildStart();
     }
-
 }
