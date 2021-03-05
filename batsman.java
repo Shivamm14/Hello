3af1519d7d106862e99e6ffa3049bfb659de882b
shivamm14 <shivammishra14nov@gmail.com>
2021-03-05 11:09:03 +0530

    wrote batsman and bowler class

---
+++ batsman.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class batsman extends Player {
+
+    public batsman(String name) {
+        super(name);
+    }
+}
---
+++ bowler.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class bowler extends Player{
+    public bowler(String name) {
+        super(name);
+    }
+}
