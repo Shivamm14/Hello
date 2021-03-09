54498be3ea076aef36096410eb687e7bf1580f86
shivamm14 <shivammishra14nov@gmail.com>
2021-03-09 15:36:13 +0530

    changed runs to enum Runs

--- Ball.java
+++ Ball.java
@@ -2,17 +2,17 @@ package com.company;
 
 // Ball class will have run and the Batsman and Bowler for this ball.
 public class Ball {
-    private int runs; // runs in 1, 2, 3, 4, 5, 6 or 7 for W
+    private Runs runs; // runs in 1, 2, 3, 4, 5, 6 or 7 for W
     private Player batsman;
     private Player bowler;
 
-    public Ball(int runs, Player batsman, Player bowler) {
+    public Ball(Runs runs, Player batsman, Player bowler) {
         this.runs = runs;
         this.batsman = batsman;
         this.bowler = bowler;
     }
 
-    public int getRuns() {
+    public Runs getRuns() {
         return runs;
     }
 
@@ -30,7 +30,7 @@ public class Ball {
                 "runs=" + runs +
                 ", batsman=" + batsman.getName() +
                 ", bowler=" + bowler.getName() +
-                '}';
+                '}' + '\n';
     }
 }
 
