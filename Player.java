181f616d4880bfbc4376e7bb1a585841616588de
shivamm14 <shivammishra14nov@gmail.com>
2021-03-09 13:32:50 +0530

    added bowling properties

--- Player.java
+++ Player.java
@@ -2,7 +2,44 @@ package com.company;
 // this stores Player info
 public class Player {
     private String name;
-    private int score;
+    private int score, bowlsBatted;
+    private int runsGiven;
+    private int wicketTaken;
+    private int ballsBowled;
+
+    public void setWicketTaken(int wicketTaken) {
+        this.wicketTaken = wicketTaken;
+    }
+
+    public void setBowlsBatted(int bowlsBatted) {
+        this.bowlsBatted = bowlsBatted;
+    }
+
+    public void setRunsGiven(int runsGiven) {
+        this.runsGiven = runsGiven;
+    }
+
+
+
+    public void setBallsBowled(int ballsBowled) {
+        this.ballsBowled = ballsBowled;
+    }
+
+    public int getBowlsBatted() {
+        return bowlsBatted;
+    }
+
+    public int getRunsGiven() {
+        return runsGiven;
+    }
+
+    public int getWicketTaken() {
+        return wicketTaken;
+    }
+
+    public int getBallsBowled() {
+        return ballsBowled;
+    }
 
     public Player(String name) {
         this.name = name;
