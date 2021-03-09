65556cce7a3ff2c4c790ec32ca0a043970c66191
shivamm14 <shivammishra14nov@gmail.com>
2021-03-09 15:34:51 +0530

    Created enum Coin and Runs

---
+++ Runs.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public enum Runs {
+    ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), WICKET(7);
+
+        private final int value;
+
+        Runs(int value) {
+            this.value = value;
+        }
+        public int value(){
+            return value;
+        }
+}
