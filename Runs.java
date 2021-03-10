7e1a6b49430074b6649663d700d2d654f34fbec7
shivamm14 <shivammishra14nov@gmail.com>
2021-03-10 11:19:06 +0530

    Added ZERO in enum Runs

--- Runs.java
+++ Runs.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public enum Runs {
-    ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), WICKET(7);
+    ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), WICKET(7);
 
         private final int value;
 
