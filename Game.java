62c6fdaf34fee6771a8d683611a31af6340bf924
shivamm14 <shivammishra14nov@gmail.com>
2021-03-18 12:09:43 +0530

    added methods initializeGame, initializeGameTeam, initializeGameTeamPlayer, initializeGamePlayer

--- Game.java
+++ Game.java
@@ -1,38 +1,59 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 // This is the Game controller class. Which will build the game, then start it.
 public class Game {
+    private String matchDate;
+    private int gameId;
+    private String location;
     private  int overs;
     private ArrayList<Over> firstInningOvers = new ArrayList<Over>();
     private ArrayList<Over> secondInningOvers = new ArrayList<Over>();
     public Game(int overs) {
         this.overs = overs;
     }
+    public Game(int over, String location, String matchDate){
+        this.overs = over;
+        this.location = location;
+        this.matchDate = matchDate;
+    }
 
     // this function will build the game by building the teams, doing the toss and then starting the match.
     public void buildStart(){
         // build teamOne
         ArrayList<Player> players = new ArrayList<Player>(Arrays.asList(
-                new Batsman("Sachin"),
-                new Batsman("Virat"),
-                new Batsman("Rohit"),
-                new Batsman("Dhoni"),
-                new Bowler("Hardik")
+                new Batsman(1, "Sachin"),
+                new Batsman(2, "Virat"),
+                new Batsman(16,"Rohit"),
+                new Batsman(4, "Dhoni"),
+                new Bowler(9, "Hardik")
         ));
-        Team teamOne = new Team("India", players);
+        Team teamOne = new Team(1,"India", players);
         // build teamTwo
         players = new ArrayList<Player>(Arrays.asList(
-                new Batsman("Ab diviliears"),
-                new Batsman("Ricky"),
-                new Bowler("Bret lee"),
-                new Bowler("Clark"),
-                new Bowler("Micheal")
+                new Batsman(14, "AB de Villiers"),
+                new Batsman(13, "Ricky"),
+                new Bowler(5, "Bret lee"),
+                new Bowler(6, "Micheal Clark"),
+                new Bowler(15, "Shane Warne")
         ));
-        Team teamTwo = new Team("Australia", players);
+        Team teamTwo = new Team(2, "Australia", players);
+        // TODO
+        // write initialize methods for game, game_team, game_player, game_team_player.
+        // team and player table will not be touched, and will be initialized manually.
+
+         initializeGame();// to initialize game.
+         initializeGameTeam(teamOne.getTeamId(), teamTwo.getTeamId()); // to initialize game_team.
+         initializeGamePlayer(teamOne); //to initialize game_player for teamOne players.
+         initializeGamePlayer(teamTwo); // for teamTwo players.
+         initializeGameTeamPlayer(teamOne); // to initialize game_team_player.
+         initializeGameTeamPlayer(teamTwo);
 
         // doing toss.
         if(coinToss() == 0){
@@ -44,13 +65,89 @@ public class Game {
         }
 
     }
+    // to make entry in game_team_player.
+    private void initializeGameTeamPlayer(Team team){
+        try{
+            Connection conn = Database.getConnection();
+            Statement st = conn.createStatement();
+
+            for(Player player : team.getPlayers()){
+                // enter each player in game_player.
+//                System.out.println(player.getPlayerId());
+                String query = String.format("INSERT INTO game_team_player (game_id, team_id, player_id)" +
+                                " VALUES(%d, %d, %d)",
+                        this.gameId, team.getTeamId(), player.getPlayerId());
+
+//                System.out.println(query); // debug.
+                st.executeUpdate(query);
+
+            }
+        }
+        catch (Exception e){
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+
+    }
+    // this method makes entry of each player in game_player. mapping each player with the game.
+    private void initializeGamePlayer(Team team){
+        try{
+            Connection conn = Database.getConnection();
+            Statement st = conn.createStatement();
+
+            for(Player player : team.getPlayers()){
+                // enter each player in game_player.
+                System.out.println(player.getPlayerId());
+                String query = String.format("INSERT INTO game_player (game_id, player_id, score, wicketsTaken," +
+                                "ballsPlayed, runsGiven, ballsBowled) VALUES(%d, %d, %d, %d, %d, %d, %d)",
+                        this.gameId, player.getPlayerId(), 0, 0, 0, 0, 0);
+
+                System.out.println(query); // debug.
+                st.executeUpdate(query);
+
+            }
+        }
+        catch (Exception e){
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+
+
+    }
+    // this method makes an entry in the game_team table with the game and team info.
+    public void initializeGameTeam(int firtTeamId, int secondTeamId) {
+        // initialize game_team table
+        try{
+            // query to enter firstTeam;
+            String query = String.format("INSERT INTO game_team (game_id, team_id, score, wickets) VALUES(%d, %d, %d, %d)",
+                    this.gameId, firtTeamId, 0, 0);
+
+            System.out.println(query);
+            Connection conn = Database.getConnection();
+            Statement st = conn.createStatement();
+
+            st.executeUpdate(query);
+            // query for second team entry.
+            query = String.format("INSERT INTO game_team (game_id, team_id, score, wickets) VALUES(%d, %d, %d, %d)",
+                    this.gameId, secondTeamId, 0, 0);
+
+            System.out.println(query);
+            st.executeUpdate(query);
+            st.close();
+        }
+        catch (Exception e){
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+    }
+
     // this function starts the match, assuming firstTeam is batting first.
     private void startMatch(Team firstTeam, Team secondTeam){
 
         // first inning
         for(int i = 0; i < overs; i++){
             Over curOver = new Over();
-            curOver.start(firstTeam, secondTeam);
+            curOver.start(gameId, firstTeam, secondTeam);
             firstInningOvers.add(curOver);
             // if all out then break.
             if(firstTeam.isAllOut()){
@@ -60,7 +157,7 @@ public class Game {
         // second inning
         for(int i = 0; i < overs; i++){
             Over curOver = new Over();
-            curOver.start(secondTeam, firstTeam, firstTeam.getScore());
+            curOver.start(gameId, secondTeam, firstTeam, firstTeam.getScore());
             secondInningOvers.add(curOver);
             // if all out or won then break;
             if(secondTeam.isAllOut() || secondTeam.getScore() > firstTeam.getScore()){
@@ -70,7 +167,35 @@ public class Game {
 
         printResult(firstTeam, secondTeam);
     }
+    // initializes game;
+    // this function makes an entry in the game table with current match info and get gameId from the table.
+    public void  initializeGame(){
 
+        // protect sql injection safe queries.
+        String query = String.format("INSERT INTO game (location, matchDate, overs) VALUES('%s', '%s', %d)",
+                this.location, this.matchDate, this.overs);
+        try{
+            System.out.println(query);
+            Connection conn = Database.getConnection();
+            Statement st = conn.createStatement();
+
+            System.out.println(query);
+            st.executeUpdate(query);
+            // get game_id
+            query = "SELECT MAX(game_id) FROM game";
+
+
+            // execute the query, and get a java resultset
+            ResultSet rs = st.executeQuery(query);
+            while(rs.next())
+             this.gameId = rs.getInt("max(game_id)");
+            st.close();
+        }
+        catch (Exception e){
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+    }
     // generate a function which returns random number between 0 and 7˳.
     public static int getRuns(){
         Random rand = new Random();
@@ -133,6 +258,9 @@ public class Game {
         return new ArrayList<Player>();
     }
 
+    public int getGameId() {
+        return gameId;
+    }
 }
 
 
